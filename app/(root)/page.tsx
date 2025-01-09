c7bcf09aa58b800e2d607cd94eb3f863e0125ea7
david2am <36830835+david2am@users.noreply.github.com>
2025-01-09 14:25:57 -0500

    feat - render some data from the server

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,22 @@
 import Hello from "@/app/components/hello";
 
-export default function Home() {
+export default async function Home() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/albums')
+  if (!response.ok) throw new Error('Failed to fetch data')
+  
+  const albums = await response.json()
+  console.log(albums)
   return (
-    <>
-      <h2>hello</h2>
-      <Hello />
-    </>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols">
+      {albums.map((album : { id : number; title : string }) => (
+        <div
+          key={album.id}
+          className="bg-white shadow-md rounded-lg p-4"
+        >
+          <h3 className="text-gray-600 text-lg font-bold mb-2">{album.title}</h3>
+          <p className="text-gray-600">Album ID: {album.id}</p>
+        </div>
+      ))}
+    </div>
   );
 }
